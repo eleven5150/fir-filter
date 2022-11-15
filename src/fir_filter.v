8fec7dbb34f795520d91922a3f1542cb9aa9c51b
Kirill <kirSM2010@gmail.com>
2022-11-16 00:17:15 +0300

    Added parameters for extensibility

--- src/fir_filter.v
+++ src/fir_filter.v
@@ -1,24 +1,31 @@
 module fir_filter(
-    input signed[7:0]input_data,
     input clk,
+    input signed [INPUT_WIDTH-1:0] input_data,
     input input_data_flag,
 
     output reg done_flag = 1,
-    output reg signed [31:0]result = 0
+    output reg signed [RESULT_WIDTH-1:0]result = 0
 );
 
+parameter INPUT_WIDTH = 8;
+parameter COEF_WIDTH = 8;
+parameter NUM_OF_TAPS = 3;
+localparam MULTIPLIED_WIDTH = (INPUT_WIDTH+COEF_WIDTH);
+localparam RESULT_WIDTH = (MULTIPLIED_WIDTH+NUM_OF_TAPS-1);
 
-reg signed [7:0] data[2:0];
-reg signed [7:0] coefs[2:0];
-reg signed [7:0] curr_coef = 0;
-reg signed [7:0] curr_data = 0;
-wire signed [15:0] mult = curr_data * curr_coef;
-reg [2:0] cnt = 0;
+reg signed [INPUT_WIDTH-1:0] data[NUM_OF_TAPS-1:0];
+reg signed [COEF_WIDTH-1:0] coefs[NUM_OF_TAPS-1:0];
+reg signed [COEF_WIDTH-1:0] curr_coef = 0;
+reg signed [INPUT_WIDTH-1:0] curr_data = 0;
+wire signed [MULTIPLIED_WIDTH-1:0] mult = curr_data * curr_coef;
+reg [$clog2(NUM_OF_TAPS):0] cnt = 0;
+
 
 
 integer i;
-initial begin
-    for (i = 0; i < 3; i = i + 1)
+initial 
+begin
+    for (i = 0; i < NUM_OF_TAPS; i = i + 1)
         begin
             data[i] = i + 1;
             coefs[i] = (i + 1) * 2;
@@ -31,7 +38,7 @@ begin
     if (input_data_flag == 1)
         begin
             done_flag <= 0;
-            for (i = 2; i >= 0; i = i - 1)
+            for (i = NUM_OF_TAPS-1; i >= 0; i = i - 1)
             begin
                 if(i != 0)
                     data[i] <= data[i - 1];
@@ -51,10 +58,10 @@ always@(posedge clk)
         else
 		    cnt <= input_data_flag;
         
-        curr_data <= cnt > 0 && cnt < 4 ? data[cnt - 1] : curr_data;
-        curr_coef <= cnt > 0 && cnt < 4 ? coefs[cnt - 1] : curr_coef;
+        curr_data <= cnt > 0 && cnt < NUM_OF_TAPS+1 ? data[cnt - 1] : curr_data;
+        curr_coef <= cnt > 0 && cnt < NUM_OF_TAPS+1 ? coefs[cnt - 1] : curr_coef;
 
-        if(cnt == 4)
+        if(cnt == NUM_OF_TAPS+1)
         begin
             cnt <= 0;        
 			done_flag <= 1;
